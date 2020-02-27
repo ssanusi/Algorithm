9c13100ffd75cc076af654cd1ac2b2f08a6c2873
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-02-27 18:20:19 +0100

    implement QuickSort

--- src/com/ssanusi/Main.java
+++ src/com/ssanusi/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] numbers = {7, 3, 5, 2, 3, 1, 5, 8};
-        var sorter = new MergeSort();
+        var sorter = new QuickSort();
         sorter.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
