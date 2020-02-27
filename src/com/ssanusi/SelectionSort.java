e6cfe5b7d96c0ee5cc7980262589b467e928d897
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-02-27 12:29:23 +0100

    implemented Selection Sort

---
+++ src/com/ssanusi/SelectionSort.java
@@ -0,0 +1,24 @@
+package com.ssanusi;
+
+public class SelectionSort {
+    public void sort(int[] array){
+        for (int i = 0; i < array.length; i++){
+            int minIndex = getMinIndex(array, i);
+            swap(array, minIndex, i);
+        }
+    }
+
+    private int getMinIndex(int[] array, int i) {
+        var minIndex = i;
+        for(int j = i  ; j < array.length; j++)
+            if(array[j] < minIndex)
+                minIndex = j;
+        return minIndex;
+    }
+
+    private void swap(int[] array, int index1, int index2){
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
