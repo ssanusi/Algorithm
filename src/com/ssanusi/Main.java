0167a1a1c9e0a45afcca27bb5b9f1a115938f26e
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-02-27 20:57:44 +0100

    implement BucketSort

--- src/com/ssanusi/Main.java
+++ src/com/ssanusi/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         int[] numbers = {7, 3, 5, 2, 3, 1, 5, 8};
-        var sorter = new CountingSort();
-        sorter.sort(numbers, 8);
+        var sorter = new BucketSort();
+        sorter.sort(numbers, 3);
         System.out.println(Arrays.toString(numbers));
     }
 }
